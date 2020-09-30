6ac6908f3a0743a51783dfdd9bbbb12afcf6c771
Jose <jozylopez27@gmail.com>
2020-09-29 22:16:49 -0500

    GQL setup

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,20 @@
-console.log('hello world!!!!!')
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Query {
+    greeting: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    greeting: () => "Hello GraphQL world!",
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server
+  .listen({ port: 9000 })
+  .then((serverInfo: any) =>
+    console.log(`listening on port ${serverInfo.url}`)
+  );
